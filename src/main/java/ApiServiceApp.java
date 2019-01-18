bd15251e71849b215dea0d922d95c9676daa9183
Travis Le <travisle247@hotmail.com>
2019-01-17 22:16:12 -0600

    Adding files in project 'ApiServices' to Git...

---
+++ src/main/java/ApiServiceApp.java
@@ -0,0 +1,13 @@
+package com.travis.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApp.class, args);
+    }
+
+}
+
